23ff4c0f392e261c65f766c1a37ae99c4fa553ac
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-08-08 22:12:32 +0530

    Profile Base Added

--- core2/urls.py
+++ core2/urls.py
@@ -1,6 +1,7 @@
 
 from django.urls import path, include
-from .views import Home
+from .views import Home, ProfileList
 urlpatterns = [
-    path('',Home.as_view())
+    path('', Home.as_view()),
+    path('profile/', ProfileList.as_view(), name='profile_list'),
 ]
--- core2/views.py
+++ core2/views.py
@@ -1,8 +1,20 @@
-from django.shortcuts import render
+
+from django.shortcuts import render,redirect
 from django.views import View
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
 
 
 class Home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/profile')
         return render(request, 'index.html')
+
+
+method_decorator(login_required,name='dispatch')
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles=request.user.profiles.all()
+        return render(request,'profileList.html',{'profiles':profiles})
