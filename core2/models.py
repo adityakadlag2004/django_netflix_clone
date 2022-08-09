88ca0cb84ac6443c843eb42830857865fe2350e9
Aditya Kadlag <adityakadlag2004@gmail.com>
2022-08-09 14:24:19 +0530

    Movie Added

--- core2/models.py
+++ core2/models.py
@@ -1,40 +1,40 @@
 from django.db import models
-import uuid
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+import uuid
 
 AGE_CHOICES=(
     ('All','All'),
     ('Kids','Kids')
 )
-MOVIE_CHOICES=(
-    ('seasonal','Seasonal'),
-    ('single','Single')
+
+MOVIE_TYPE=(
+    ('single','Single'),
+    ('seasonal','Seasonal')
 )
 
-
 class CustomUser(AbstractUser):
-    profiles=models.ManyToManyField('Profile',blank=True)
+    profiles=models.ManyToManyField('Profile')
 
 
 class Profile(models.Model):
-    name=models.CharField(max_length=200)
-    age_limit=models.CharField(max_length=10,choices=AGE_CHOICES)
-    uuid=models.UUIDField(default=uuid.uuid4)
+    name=models.CharField(max_length=225)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
 
+
+    def __str__(self):
+        return self.name +" "+self.age_limit
 
 class Movie(models.Model):
-    title=models.CharField(max_length=400)
-    description=models.TextField(blank=True,null=True)
-    created=models.DateTimeField(auto_now_add=True)
-    uuid=models.UUIDField(default=uuid.uuid4)
-    type=models.CharField(max_length=10,choices=MOVIE_CHOICES)
+    title:str=models.CharField(max_length=225)
+    description:str=models.TextField()
+    created =models.DateTimeField(auto_now_add=True)
+    uuid=models.UUIDField(default=uuid.uuid4,unique=True)
+    type=models.CharField(max_length=10,choices=MOVIE_TYPE)
     videos=models.ManyToManyField('Video')
-    flyer=models.ImageField(upload_to='flyers')
-    age_limit=models.CharField(max_length=10,choices=AGE_CHOICES)
-
+    flyer=models.ImageField(upload_to='flyers',blank=True,null=True)
+    age_limit=models.CharField(max_length=5,choices=AGE_CHOICES,blank=True,null=True)
 
 class Video(models.Model):
-    title=models.CharField(max_length=400,blank=True,null=True)
+    title:str = models.CharField(max_length=225,blank=True,null=True)
     file=models.FileField(upload_to='movies')
-
--- core2/views.py
+++ core2/views.py
@@ -1,9 +1,11 @@
 
-from django.shortcuts import render,redirect
+from venv import create
+from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-
+from .forms import Profile, ProfileForm
+from .models import Movie
 
 
 class Home(View):
@@ -13,14 +15,90 @@ class Home(View):
         return render(request, 'index.html')
 
 
-method_decorator(login_required,name='dispatch')
+method_decorator(login_required, name='dispatch')
+
+
 class ProfileList(View):
     def get(self, request, *args, **kwargs):
-        profiles=request.user.profiles.all()
-        return render(request,'profileList.html',{'profiles':profiles})
+        profiles = request.user.profiles.all()
+        return render(request, 'profileList.html', {'profiles': profiles})
 
 
-
+@method_decorator(login_required,name='dispatch')
 class ProfileCreate(View):
     def get(self,request,*args, **kwargs):
-        return render(request,'profileCreate.html')
+        form=ProfileForm()
+
+        return render(request,'profileCreate.html',{
+            'form':form
+        })
+
+    def post(self,request,*args, **kwargs):
+        form=ProfileForm(request.POST or None)
+
+       
+        if form.is_valid():
+            print(form.cleaned_data)
+            profile = Profile.objects.create(**form.cleaned_data)
+            if profile:
+                request.user.profiles.add(profile)
+                return redirect('/profile')
+
+        return render(request,'profileCreate.html',{
+            'form':form
+        })
+
+
+@method_decorator(login_required,name='dispatch')
+class Watch(View):
+    def get(self,request,profile_id,*args, **kwargs):
+        try:
+            profile=Profile.objects.get(uuid=profile_id)
+
+            movies=Movie.objects.filter(age_limit=profile.age_limit)
+
+            try:
+                showcase=movies[0]
+            except :
+                showcase=None
+            
+
+            if profile not in request.user.profiles.all():
+                return redirect(to='/   profile_list')
+            return render(request,'movieList.html',{
+            'movies':movies,
+            'show_case':showcase
+            })
+        except Profile.DoesNotExist:
+            return redirect(to='core:profile_list')
+
+
+
+@method_decorator(login_required,name='dispatch')
+class ShowMovieDetail(View):
+    def get(self,request,movie_id,*args, **kwargs):
+        try:
+            
+            movie=Movie.objects.get(uuid=movie_id)
+
+            return render(request,'movieDetail.html',{
+                'movie':movie
+            })
+        except Movie.DoesNotExist:
+            return redirect('core:profile_list')
+
+@method_decorator(login_required,name='dispatch')
+class ShowMovie(View):
+    def get(self,request,movie_id,*args, **kwargs):
+        try:
+            
+            movie=Movie.objects.get(uuid=movie_id)
+
+            movie=movie.videos.values()
+            
+
+            return render(request,'showMovie.html',{
+                'movie':list(movie)
+            })
+        except Movie.DoesNotExist:
+            return redirect('core:profile_list')
